0d08c78fcf8acc1b6be1de985796b58c46ceabac
Mert <32747250+sailor002@users.noreply.github.com>
2023-10-24 11:10:35 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -278,8 +278,10 @@ def song_cover_pipeline(song_input, voice_model, pitch_change, keep_files,
 
         #if not os.path.exists(song_dir):
         #    os.makedirs(song_dir)
+        
         orig_song_path, vocals_path, instrumentals_path, main_vocals_path, backup_vocals_path, main_vocals_dereverb_path = preprocess_song(song_input, mdx_model_params, is_webui, input_type, progress)
 
+        """
         else:
             vocals_path, main_vocals_path = None, None
             paths = get_audio_paths(song_dir)
@@ -289,6 +291,7 @@ def song_cover_pipeline(song_input, voice_model, pitch_change, keep_files,
                 orig_song_path, vocals_path, instrumentals_path, main_vocals_path, backup_vocals_path, main_vocals_dereverb_path = preprocess_song(song_input, mdx_model_params, song_id, is_webui, input_type, progress)
             else:
                 orig_song_path, instrumentals_path, main_vocals_dereverb_path, backup_vocals_path = paths
+        """
 
         pitch_change = pitch_change * 12 + pitch_change_all
         ai_vocals_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]}_{voice_model}_p{pitch_change}_i{index_rate}_fr{filter_radius}_rms{rms_mix_rate}_pro{protect}_{f0_method}{"" if f0_method != "mangio-crepe" else f"_{crepe_hop_length}"}.wav')
