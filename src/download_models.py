d89aff213e93746712e1e4d0ae5dcd9a56c04360
Mert <32747250+sailor002@users.noreply.github.com>
2024-02-22 18:13:15 +0300

    Update download_models.py
    Added runpod volume

--- src/download_models.py
+++ src/download_models.py
@@ -6,8 +6,9 @@ MDX_DOWNLOAD_LINK = 'https://github.com/TRvlvr/model_repo/releases/download/all_
 RVC_DOWNLOAD_LINK = 'https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-mdxnet_models_dir = BASE_DIR / 'mdxnet_models'
-rvc_models_dir = BASE_DIR / 'rvc_models'
+RUNPOD_VOLUME_DIR = '/runpod-volume'
+mdxnet_models_dir = RUNPOD_VOLUME_DIR / 'mdxnet_models'
+rvc_models_dir = RUNPOD_VOLUME_DIR / 'rvc_models'
 
 
 def dl_model(link, model_name, dir_name):
